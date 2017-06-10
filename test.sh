7df6c51797bef8486a201e2690b5f766a5c465d0
Gabriel Csapo <gabecsapo@gmail.com>
2017-06-09 21:13:50 -0700

    fixes test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 REPOS='nolanlawson/tiny-queue lodash/lodash substack/node-browserify strongloop/express facebook/react gulpjs/gulp pouchdb/pouchdb'
 
 RESULTS_FILE=$(pwd)/results.md
 
-function timeIt () { { time npm install > /dev/null 2>&1 ; } 2>&1 | grep real | awk '{print $2}' ; }
+timeIt() { { time npm install > /dev/null 2>&1 ; } 2>&1 | grep real | awk '{print $2}' ; }
 
 rm -fr $RESULTS_FILE workspace
 touch $RESULTS_FILE
